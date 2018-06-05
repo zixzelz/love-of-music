a815741cb0b6754e442871aee691537d5d43106f
Ruslan Maslouski <zixzelz@gmail.com>
2018-06-05 15:26:46 +0300

    Initial Commit

---
+++ love-of-music/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  love-of-music
+//
+//  Created by Ruslan Maslouski on 6/5/18.
+//  Copyright © 2018 Ruslan Maslouski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
